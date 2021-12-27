93e141b48fe8ad1a8e130633d25fe43cf9c60661
gbarros1994 <55881896+gbarros1994@users.noreply.github.com>
2021-12-27 10:34:08 -0300

    first commit

---
+++ pages/product/_product.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{ this.slug }}a</h1>
+</template>
+
+<script>
+  export default {
+    async asyncData({ params }) {
+      console.log(params)
+      const slug = params // When calling /abc the slug will be "abc"
+      return { slug }
+    }
+  }
+</script>
---
+++ pages/product/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        <nuxt-link :to="'product/'+product.id">{{ product.name }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'Bola de basquete',
+          description: 'Apenas uma bola de basquete'
+        }
+      ]
+    }
+  }
+}
+</script>
